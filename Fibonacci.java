cf87a285ff77e73c8dcc462d53b572cb38eb1913
gulamyazdani04 <124505505+gulamyazdani04@users.noreply.github.com>
2023-02-07 22:45:54 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,7 +11,7 @@ public class Fibonacci {
         int a = 0, b = 1, c;
         System.out.print(a + " " + b);
         for (int i = 2; i < n; i++) 
-		{
+	{
             c = a + b;
             System.out.print(" " + c);
             a = b;
